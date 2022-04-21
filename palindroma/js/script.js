cc50f36b601d2f6e9c6f5913df574f7d0b82e453
Lolloc99 <lolloc99@yahoo.it>
2022-04-21 14:22:37 +0200

    Esercizio palindroma funzionante, controllo miglioramenti

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -4,3 +4,20 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
+// Chiedo parola all'utente
+let userWord = prompt("Inserisci una parola");
+let result = false;
+
+console.log(userWord);
+palindrometro(userWord);
+
+if (palindrometro(userWord) === userWord) {
+    result = true;
+}
+
+console.log("Verifica se la parola inserita è palindroma: ", result);
+
+// Funzione per check palindroma
+function palindrometro(word){
+    return word.split("").reverse().join("");
+}
